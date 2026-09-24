3e8fed5961097c7a0a44c9b463ef7f6715c0f872
QCerberusQ <113352008+QCerberusQ@users.noreply.github.com>
2026-09-24 18:50:42 +0300

    surya: Remove FOX_VANILLA_BUILD export from vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -36,7 +36,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
 
-	export FOX_VANILLA_BUILD=1
 	export FOX_ALLOW_EARLY_SETTINGS_LOAD=1
 	export FOX_SETTINGS_ROOT_DIRECTORY=/persist/recovery
 	export FOX_MISCELLANEOUS_ROOT_DIRECTORY=/sdcard
